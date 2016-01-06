9def1106188811b69fb38da5cfe7cf47638d7ad5
Tomas Deml <deml.tomas@gmail.com>
2016-01-06 19:59:36 +0100

    Finished npm packaging and published v1.0.0

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@
 var merge = require('merge2');
 var ts = require('gulp-typescript');
 var jasmine = require('gulp-jasmine');
-var exec = require('child_process').exec
+var rename = require("gulp-rename");
 
-var releaseDir = 'release';
-var specsDir = 'spec-release';
+var releaseDir = '_package';
+var specReleaseDir = '_spec-release';
 var tsConfigPath = 'tsconfig.json';
 
-gulp.task('default', ['compileSrc', 'test']);
+gulp.task('default', ['compileSrc', 'runTests', 'copyOtherPackageFilesToRelease']);
 
 gulp.task('compileSrc', function (cb) {
     var tsProject = ts.createProject(tsConfigPath);
@@ -18,12 +18,12 @@ gulp.task('compileSrc', function (cb) {
             .pipe(gulp.dest(releaseDir)),
         tsResult.js
             .pipe(gulp.dest(releaseDir))
-            .pipe(gulp.dest(specsDir))
+            .pipe(gulp.dest(specReleaseDir))
     ]);
 });
 
-gulp.task('test', ['copyJasmineCore'], function (cb) {
-    return gulp.src(specsDir + '/spec/**/*.spec.js')
+gulp.task('runTests', ['copyJasmineCore'], function (cb) {
+    return gulp.src(specReleaseDir + '/spec/**/*.spec.js')
         .pipe(jasmine({
             verbose: true,
             includeStackTrace: false
@@ -32,5 +32,16 @@ gulp.task('test', ['copyJasmineCore'], function (cb) {
 
 gulp.task('copyJasmineCore', function () {
     return gulp.src('node_modules/jasmine-core/lib/jasmine-core/**/*')
-        .pipe(gulp.dest(specsDir + '/jasmine-core'));
+        .pipe(gulp.dest(specReleaseDir + '/jasmine-core'));
+});
+
+gulp.task('copyOtherPackageFilesToRelease', function (cb) {
+    return merge([
+        gulp.src('./package.json').pipe(gulp.dest(releaseDir)),
+        gulp.src('./npmignore.res')
+            .pipe(rename('.npmignore'))
+            .pipe(gulp.dest(releaseDir)),
+        gulp.src('./LICENSE').pipe(gulp.dest(releaseDir)),
+        gulp.src('./README.md').pipe(gulp.dest(releaseDir))
+    ]);
 });
